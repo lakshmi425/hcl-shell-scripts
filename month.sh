199dbc3a8dfddfbf593cc5482b7777d59ed6e2fe
lakshmi <lakshmi.pokuru1994@gmail.com>
2021-08-12 11:55:11 +0000

    this is for month

---
+++ month.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+case $n in 
+	1)
+		echo  "you select january";;
+	2)
+		echo "you select  february";;
+	3)
+		echo "you select march";;
+	4)
+		echo "you select april";;
+	5)
+		echo "you select may ";;
+	6)
+		echo "you select june";;
+	7)
+		echo "you select july";;
+
+	8)
+		echo "you select august";;
+	9)
+		echo "you select september";;
+	10)
+		echo "you select october";;
+	11)
+		echo "you entered november";;
+	12)
+		echo "you entered december";;
+
+	esac
